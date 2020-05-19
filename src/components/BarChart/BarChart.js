860a185c774dffdc29744ef95638e1ee9e09e37e
Igor <thekreizo@gmail.com>
2020-05-19 18:08:25 +0300

    bar chart

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -14,26 +14,13 @@ BarChart.defaultProps = {
   width: 1200,
   height: 300,
   data: [
-    {
-      name: "Численность населения 1",
-      inner: "20",
-      scen: "40",
-    },
-    {
-      name: "Численность населения 2",
-      inner: "10",
-      scen: "35",
-    },
-    {
-      name: "Численность населения 3",
-      inner: "25",
-      scen: "55",
-    },
-    {
-      name: "Численность населения 4",
-      inner: "50",
-      scen: "50",
-    },
+    { name: "Численность населения", apples: 3840, bananas: 1920, cherries: -1960, dates: -400 },
+    { name: "ВГП на дущу населения", apples: 1600, bananas: 1440, cherries: -960, dates: -400 },
+    { name: "ВГП", apples: 640, bananas: 960, cherries: -640, dates: -600 },
+    { name: "Доходы бюджетов", apples: 320, bananas: 480, cherries: -640, dates: -400 },
+    { name: "Прибыль предприятий", apples: 320, bananas: 480, cherries: -640, dates: -400 },
+    { name: "Инвестиции", apples: 320, bananas: 480, cherries: -640, dates: -400 },
+    { name: "Жилобеспеченность", apples: 320, bananas: 480, cherries: -640, dates: -400 },
   ],
-  colors: ["#FFC34F", "#73CFCA"],
+  colors: ["#FFC34F", "#73CFCA", "#61ff4f", "#b673cf"],
 };
--- src/components/BarChart/useDraw.js
+++ src/components/BarChart/useDraw.js
@@ -1,129 +1,94 @@
 import * as d3 from "d3";
 import { useCallback } from "react";
 
+function stackMin(serie) {
+  return d3.min(serie, (d) => d[0]);
+}
+
+function stackMax(serie) {
+  return d3.max(serie, (d) => d[1]);
+}
+
 export function useDraw(props) {
   const ref = useCallback(
     (node) => {
       if (node !== null) {
-        const { height, labels, colors } = props;
-
-        const data = [
-          {
-            name: "CA",
-            "<10": 5038433,
-            "10-19": 5170341,
-            "20-29": 5809455,
-            "30-39": 5354112,
-            "40-49": 5179258,
-            "50-59": 5042094,
-            "60-69": 3737461,
-            "70-79": 2011678,
-            "≥80": 1311374,
-            total: 38654206,
-          },
-          {
-            name: "TX",
-            "<10": 3983091,
-            "10-19": 3910528,
-            "20-29": 3946447,
-            "30-39": 3770534,
-            "40-49": 3545746,
-            "50-59": 3344930,
-            "60-69": 2431494,
-            "70-79": 1291486,
-            "≥80": 732179,
-            total: 26956435,
-          },
-          {
-            name: "FL",
-            "<10": 2211012,
-            "10-19": 2331102,
-            "20-29": 2597830,
-            "30-39": 2416176,
-            "40-49": 2575576,
-            "50-59": 2762983,
-            "60-69": 2404659,
-            "70-79": 1615547,
-            "≥80": 1019566,
-            total: 19934451,
-          },
-          {
-            name: "NY",
-            "<10": 2319945,
-            "10-19": 2445591,
-            "20-29": 2894266,
-            "30-39": 2605355,
-            "40-49": 2617327,
-            "50-59": 2755620,
-            "60-69": 2095207,
-            "70-79": 1160055,
-            "≥80": 804091,
-            total: 19697457,
-          },
-        ];
-
+        const { data, height, labels, colors } = props;
         const width = Math.min(props.width, node.getBoundingClientRect().width);
-        const margin = { top: 10, right: 10, bottom: 20, left: 40 };
-
-        const formatValue = (x) => (isNaN(x) ? "N/A" : x.toLocaleString("en"));
-
         /** SVG **/
         d3.select(node).select("svg").remove();
+
+        const series = d3.stack().keys(["apples", "bananas", "cherries", "dates"]).offset(d3.stackOffsetDiverging)(
+          data,
+        );
+
         const svg = d3.select(node).append("svg").attr("viewBox", [0, 0, width, height]);
-
-        const series = d3
-          .stack()
-          .keys(["<10", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "≥80"])(data)
-          .map((d) => (d.forEach((v) => (v.key = d.key)), d));
-
-        const color = d3
-          .scaleOrdinal()
-          .domain(series.map((d) => d.key))
-          .range(d3.schemeSpectral[series.length])
-          .unknown("#ccc");
+        const margin = { top: 20, right: 30, bottom: 30, left: 60 };
 
         const x = d3
           .scaleBand()
           .domain(data.map((d) => d.name))
-          .range([margin.left, width - margin.right])
+          .rangeRound([margin.left, width - margin.right])
           .padding(0.1);
-
-        const xAxis = (g) =>
-          g
-            .attr("transform", `translate(0,${height - margin.bottom})`)
-            .call(d3.axisBottom(x).tickSizeOuter(0))
-            .call((g) => g.selectAll(".domain").remove());
 
         const y = d3
           .scaleLinear()
-          .domain([0, d3.max(series, (d) => d3.max(d, (d) => d[1]))])
+          .domain([d3.min(series, stackMin), d3.max(series, stackMax)])
           .rangeRound([height - margin.bottom, margin.top]);
-
-        const yAxis = (g) =>
-          g
-            .attr("transform", `translate(${margin.left},0)`)
-            .call(d3.axisLeft(y).ticks(null, "s"))
-            .call((g) => g.selectAll(".domain").remove());
 
         svg
           .append("g")
+          .attr("transform", "translate(0," + y(d3.min(series, stackMin)) + ")")
+          .call(d3.axisBottom(x))
+          .selectAll("line, .domain")
+          .remove();
+
+        svg
+          .append("g")
+          .attr("transform", "translate(" + margin.left + ",0)")
+          .call(d3.axisLeft(y).tickFormat((d) => `${d}%`))
+          .call((g) => {
+            g.selectAll(".domain").remove();
+            g.selectAll("line")
+              .attr("stroke-width", 1)
+              .attr("x2", width)
+              .attr("x1", 0)
+              .attr("x2", width)
+              .attr("shape-rendering", "crispEdges")
+              .each((d, i, elements) => {
+                if (d === 0) {
+                  elements[i].remove();
+                }
+              });
+          });
+
+        svg
+          .append("g")
+          .attr("class", "bar-container")
           .selectAll("g")
           .data(series)
-          .join("g")
-          .attr("fill", (d) => color(d.key))
+          .enter()
+          .append("g")
+          .attr("fill", (d, i) => colors[i])
           .selectAll("rect")
           .data((d) => d)
-          .join("rect")
-          .attr("x", (d, i) => x(d.data.name))
+          .enter()
+          .append("rect")
+          .attr("width", x.bandwidth)
+          .attr("x", (d) => x(d.data.name))
           .attr("y", (d) => y(d[1]))
-          .attr("height", (d) => y(d[0]) - y(d[1]))
-          .attr("width", x.bandwidth())
-          .append("title")
-          .text((d) => `${d.data.name} ${d.key} ${formatValue(d.data[d.key])}`);
+          .attr("height", (d) => y(d[0]) - y(d[1]));
 
-        svg.append("g").call(xAxis);
-
-        svg.append("g").call(yAxis);
+        svg
+          .append("line")
+          .attr("transform", `translate(${margin.left},${y(0)})`)
+          .attr("class", "zero-tick")
+          .attr("stroke-width", 1)
+          .attr("stroke", "black")
+          .attr("x2", width)
+          .attr("x1", 0)
+          .attr("x2", width)
+          .attr("shape-rendering", "crispEdges");
       }
     },
     [props],
